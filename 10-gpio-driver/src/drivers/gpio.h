c3a6d96d9ce05019ac449cf572e6b0c69f552935
Maksym Palii <maksympalii.dev@gmail.com>
2026-02-21 21:16:21 +0200

    m1284-10: GPIO driver

--- 10-gpio-driver/src/drivers/gpio.h
+++ 10-gpio-driver/src/drivers/gpio.h
@@ -1,89 +1,3 @@
-#!/bin/bash
-# C project initializer
-
-if [ -e Makefile ] || [ -e src/main.c ] || [ -e src/drivers/gpio.h ]; then
-    echo "Project already initialized"
-    exit 1
-fi
-
-mkdir -p src/drivers
-
-# main.c
-cat > "src/main.c" <<EOF
-/**
- * @author $(whoami)
- * @brief 
- * @version 1.0
- * @date $(date '+%Y-%m-%d')
- */
-
-#include "drivers/gpio.h"
-
-int main(void)
-{
-    while(1)
-    {
-    
-    }
-
-    return 0;
-}
-EOF
-
-# Makefile
-cat > "Makefile" <<'EOF'
-# Makefile
-SOURCE       = ./src/main.c
-DESTINATION  = firmware
-DEVICE       = atmega1284
-PROGRAMMER   = usbasp-clone
-
-default: build
-
-build: clean 
-	@mkdir target
-	@avr-gcc -mmcu=$(DEVICE) -Os -Wall -Wextra -Wconversion -o ./target/$(DESTINATION).elf $(SOURCE)
-	@avr-objcopy --input-target elf32-avr --output-target ihex --verbose ./target/$(DESTINATION).elf ./target/$(DESTINATION).hex
-	@echo "INFO: build finished"
-
-clean:
-	@rm --force --recursive --verbose ./target
-	@echo "INFO: clean finished"
-
-check:
-	@avrdude -c $(PROGRAMMER) -p $(DEVICE)
-
-erease:
-	@avrdude -c $(PROGRAMMER) -p $(DEVICE) -e
-
-upload:
-	@avrdude -c $(PROGRAMMER) -p $(DEVICE) -U flash:w:./target/$(DESTINATION).hex:i
-
-disasm: ./target/$(DESTINATION).elf
-	@avr-objdump -d ./target/$(DESTINATION).elf
-
-size:	./target/$(DESTINATION).elf
-	@avr-size --format=AVR --mcu=$(DEVICE) ./target/$(DESTINATION).elf 
-
-fuses-default:
-	@avrdude -c $(PROGRAMMER) -p $(DEVICE) -U lfuse:w:0x62:m -U hfuse:w:0x99:m -U efuse:w:0xff:m 
-
-fuses-no-divider-int-rc:
-	@avrdude -c $(PROGRAMMER) -p $(DEVICE) -U lfuse:w:0xe2:m
-
-fuses-no-divider-ext-osc:
-	@avrdude -c $(PROGRAMMER) -p $(DEVICE) -U lfuse:w:0xff:m
-
-fuses-with-divider-ext-osc:
-	@avrdude -c $(PROGRAMMER) -p $(DEVICE) -U lfuse:w:0x7f:m
-
-fuses-jtag-disable:
-	@avrdude -c $(PROGRAMMER) -p $(DEVICE) -U hfuse:w:0xd9:m
-
-EOF
-
-# gpio.h
-cat > "src/drivers/gpio.h" <<EOF
 #ifndef GPIO_H_
 #define GPIO_H_
 
@@ -220,6 +134,7 @@ cat > "src/drivers/gpio.h" <<EOF
 #define gpio_set_input(GPIO)    (*_DDR_ADDRESS(GPIO) &= (uint8_t) ~_GPIO_PIN_BIT(GPIO))
 #define gpio_high(GPIO)         (*_PORT_ADDRESS(GPIO) |= _GPIO_PIN_BIT(GPIO))
 #define gpio_low(GPIO)          (*_PORT_ADDRESS(GPIO) &= (uint8_t) ~_GPIO_PIN_BIT(GPIO))
+// #define gpio_toggle(GPIO)       (*_PORT_ADDRESS(GPIO) ^= _GPIO_PIN_BIT(GPIO))
 #define gpio_toggle(GPIO)       (*_PIN_ADDRESS(GPIO) = _GPIO_PIN_BIT(GPIO))
 #define gpio_get_input(GPIO)    (*_PIN_ADDRESS(GPIO) & _GPIO_PIN_BIT(GPIO))
 
@@ -236,4 +151,3 @@ cat > "src/drivers/gpio.h" <<EOF
     __asm__ volatile ("sbi %0, %1" :: "I" (_PIN_ADDRESS(GPIO) - __SFR_OFFSET), "I" (GPIO & _GPIO_PIN_MASK))
 
 #endif /* GPIO_H_ */
-EOF
